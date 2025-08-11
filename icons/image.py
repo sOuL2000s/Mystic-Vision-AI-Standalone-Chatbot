8941caccb08df128526a6ca9ae57b2086b1ede68
sOuL2000s <souparnopaulreborn@gmail.com>
2025-08-11 19:47:20 +0530

    updated

--- icons/image.py
+++ icons/image.py
@@ -1,6 +1,6 @@
 from PIL import Image
 
-img = Image.open("logo.png")
+img = Image.open("D:/72 projects of python/Mystic Vision AI (Standalone AI Chatbot)/logo.png")
 sizes = [72, 96, 128, 144, 152, 192, 384, 512] # Desired square sizes
 
 for size in sizes:
